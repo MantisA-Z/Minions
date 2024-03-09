57110158c35e263673a4d9a2563dc95637f46dc7
MantisA-Z <160739281+MantisA-Z@users.noreply.github.com>
2024-03-09 23:37:57 +0530

    commit

---
+++ divce_roll game/script - Copy.js
@@ -0,0 +1,20 @@
+function roll_dice(){
+const usr_input = document.getElementById('in').value;
+const result = document.getElementById('result');
+const images = document.getElementById('images');
+let values = [];
+let imgs = [];
+
+
+for (let i = 0; i < usr_input; i++){
+    let value = Math.floor(Math.random() * 6) + 1;
+    values.push(value);
+    imgs.push(`<img src="png/${value}.png">`);
+ };
+ 
+ result.textContent = `Dice: ${values.join(', ')}`;
+ images.innerHTML = imgs.join('');
+ localStorage.setItem('save_img', images);
+ localStorage.setItem('save_result', result);
+
+}
